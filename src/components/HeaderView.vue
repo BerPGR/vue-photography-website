484d3379bcb8aa9ec7069c67faf3f1698f893736
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-04-04 15:41:37 -0300

    Created Header

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,6 +1,24 @@
 <template>
   <v-container>
-    Header
+    <v-toolbar flat>
+      <v-toolbar-title>Photography</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>
+          HOME
+        </v-btn>
+
+        <v-btn text>
+          ABOUT
+        </v-btn>
+
+        <v-btn text>
+          CONTACT
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </v-container>
 </template>
 
